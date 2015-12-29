ca16103c2cef1a64263aa5f89d255eaa23c10cf5
Younghyun Jo <younghyun.jo@daliworks.net>
2015-12-30 01:50:08 +0900

    delete unused node modules, th02 modifed

--- gw_b0/th02.js
+++ gw_b0/th02.js
@@ -23,102 +23,125 @@ function Th02() {
 
 Th02.prototype.getTemperature = function (cb) {
   var self = this;
-    async.waterfall([
-      function (done) {
-        self.mutex.lock(function () {
-          done();
+  async.waterfall([
+    function (done) {
+      self.mutex.lock(function () {
+        return done();
         });
-      },
-      function (done) {
-        self.i2c.writeBytes(TH02_REG_CONFIG, [TH02_CMD_MEASURE_TEMP], function (err) {
+    },
+    function (done) {
+      self.i2c.writeBytes(TH02_REG_CONFIG, [TH02_CMD_MEASURE_TEMP], function (err) {
+        if (err) {
+          console.log('i2c write failed. reg:%x cmd:%x', TH02_REG_CONFIG, TH02_CMD_MEASURE_TEMP);
+        }
+
+        return done (err);
+      });
+    },
+    function (done) {
+      var timer = setInterval(function () {
+        self.i2c.readBytes(TH02_REG_STATUS, 1, function (err, res) {
           if (err) {
-            console.log('i2c write failed. reg:%x cmd:%x', 
-              TH02_REG_CONFIG, TH02_CMD_MEASURE_TEMP);
+            clearInterval(timer);
+            console.log('readBytes failed. reg:%x', TH02_REG_STATUS);
+            return done(err);
           }
-   
-          done (err);
+          if (!(res & TH02_STATUS_RDY_MASK)) {
+            clearInterval(timer);
+            return done(err);
+          }
         });
-      },
-      function (done) {
-        var timer = setInterval(function () {
-          self.i2c.readBytes(TH02_REG_STATUS, 1, function (err, res) {
-            if (err) {
-              done(err);
-              clearInterval(timer);
-            }
-            if (res & TH02_STATUS_RDY_MASK) {
-            }
-            else {
-              done(err);
-              clearInterval(timer);
-            }
-          });
-        }, 1000);
-      },
-      function (done) {
-        self.i2c.readBytes(TH02_REG_DATA_H, 2, function (err, res) {
-          var temperature = (res[0] << 8) | (res[1]);
-          temperature >>= 2;
-          temperature = (parseFloat(temperature) / 32.0) - 50.0;
-          console.log(temperature);
-          done();
-        });
-      },
-      function (done) {
-        self.mutex.unlock();
-        done();
-      }]);
+      }, 1000);
+    },
+    function (done) {
+      self.i2c.readBytes(TH02_REG_DATA_H, 2, function (err, res) {
+        if (err) {
+            console.log('readBytes failed. reg:%x', TH02_REG_DATA_H);
+            return done(err);
+        }
+
+        var rawTemperature = (res[0] << 8) | (res[1]);
+        rawTemperature >>= 2;
+        rawTemperature = (parseFloat(rawTemperature) / 32.0) - 50.0;
+        return done(null, rawTemperature);
+      });
+    }],
+    function (err, temperature) {
+      self.mutex.unlock();
+      if (err) {
+        console.log('getTemperature err');
+        cb && cb(err);
+        return;
+      }
+
+      cb && cb(null, temperature);
+    }
+  );
 };
 
-Th02.prototype.getHuminity = function (cb) {
+Th02.prototype.getHumidity = function (cb) {
   var self = this;
-  this.mutex.lock(function () {
-    async.waterfall([
-      function (done) {
-        self.i2c.writeBytes(TH02_REG_CONFIG, [TH02_CMD_MEASURE_HUMI], function (err) {
+  async.waterfall([
+    function (done) {
+      self.mutex.lock(function () {
+        return done();
+        });
+    },
+    function (done) {
+      self.i2c.writeBytes(TH02_REG_CONFIG, [TH02_CMD_MEASURE_HUMI], function (err) {
+        if (err) {
+          console.log('i2c write failed. reg:%x cmd:%x', TH02_REG_CONFIG, TH02_CMD_MEASURE_HUMI);
+        }
+
+        return done (err);
+      });
+    },
+    function (done) {
+      var timer = setInterval(function () {
+        self.i2c.readBytes(TH02_REG_STATUS, 1, function (err, res) {
           if (err) {
-            console.log('i2c write failed. reg:%x cmd:%x', 
-              TH02_REG_CONFIG, TH02_CMD_MEASURE_TEMP);
+            clearInterval(timer);
+            console.log('readBytes failed. reg:%x', TH02_REG_STATUS);
+            return done(err);
           }
-   
-          done (err);
+          if (!(res & TH02_STATUS_RDY_MASK)) {
+            clearInterval(timer);
+            return done(err);
+          }
         });
-      },
-      function (done) {
-        var timer = setInterval(function () {
-          self.i2c.readBytes(TH02_REG_STATUS, 1, function (err, res) {
-            if (err) {
-              done(err);
-              clearInterval(timer);
-            }
-            if (res & TH02_STATUS_RDY_MASK) {
-            }
-            else {
-              done(err);
-              clearInterval(timer);
-            }
-          });
-        }, 1000);
-      },
-      function (done) {
-        self.i2c.readBytes(TH02_REG_DATA_H, 2, function (err, res) {
-          var humidity = (res[0] << 8) | (res[1]);
-          humidity >>= 4;
-          humidity = (parseFloat(humidity) / 16.0) - 24.0;
-          console.log(humidity);
-          done();
-        });
-      }, 
-      function (done) {
-        self.mutex.unlock();
-        done();
-      }]);
-  });
+      }, 1000);
+    },
+    function (done) {
+      self.i2c.readBytes(TH02_REG_DATA_H, 2, function (err, res) {
+        if (err) {
+            console.log('readBytes failed. reg:%x', TH02_REG_DATA_H);
+            return done(err);
+        }
+
+        var rawHumidity = (res[0] << 8) | (res[1]);
+        rawHumidity >>= 4;
+        rawHumidity = (parseFloat(rawHumidity) / 16.0) - 24.0;
+        return done(null, rawHumidity);
+      });
+    }],
+    function (err, humidity) {
+      self.mutex.unlock();
+      if (err) {
+        console.log('getTemperature err');
+        cb && cb(err);
+        return;
+      }
+
+      cb && cb(null, humidity);
+    }
+  );
 };
+
+module.exports = Th02;
 
 if (require.main === module) {
   var th02 = new Th02();
-  th02.getTemperature();
-  th02.getHuminity();
+  th02.getTemperature(function (err, t) {console.log(t)});
+  th02.getHumidity(function (err, t) {console.log(t)});
 }
 
